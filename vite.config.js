3ef9a31a13f7071961153468412dca8bad95f376
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-14 11:35:28 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Aoi/', // Thay bằng tên repository của bạn
