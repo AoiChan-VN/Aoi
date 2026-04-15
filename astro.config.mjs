967720eaf9bd635a28b55aaecf98bec41c07fb48
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-15 20:06:50 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://aoichan-vn.github.io/Aoi/',
+  site: 'https://aoichan-vn.github.io',
   base: '/Aoi/',
-  integrations: [react()],
   output: 'static',
-  build: {
-    inlineStylesheets: 'auto'
-  }
 });
