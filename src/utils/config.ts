b303beb9e189506a5b455436b722a40122e5bd13
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-15 19:53:07 +0700

    Create config.ts

---
+++ src/utils/config.ts
@@ -0,0 +1,10 @@
+export const SITE = {
+  name: "AoiChan",
+  url: "https://aoichan-vn.github.io/Aoi/",
+  description: "AoiChan personal website"
+};
+
+export const SETTINGS = {
+  defaultTheme: "dark",
+  lang: "en"
+}; 
