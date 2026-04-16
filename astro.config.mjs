2d0bf7d06bcd36b901e14d2f48d888efb8deaf6e
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-16 14:33:11 +0700

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  site: 'https://aoichan-vn.github.io',
-  base: '/',
-  integrations: [tailwind()],
-  output: 'static'
-});
