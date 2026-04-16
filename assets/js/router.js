9e950ee2c1505c4a7643a2a94577140e7457a23f
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-16 18:38:09 +0700

    Update router.js

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,13 +1,11 @@
 function router() {
   const path = location.hash.slice(1) || "/";
 
-  if (path === "/") {
-    document.getElementById("app").innerHTML = "<h2>Welcome</h2>";
-  }
-
   if (path.startsWith("/post/")) {
     const slug = path.split("/")[2];
+    CURRENT = slug;
     loadPost(slug);
+    renderPosts();
   }
 }
 
