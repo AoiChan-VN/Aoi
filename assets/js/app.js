fe9dac3607e528ebe149318d1d027bab686fe33c
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-17 00:04:48 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,12 @@
 (async () => {
-  const lang = localStorage.getItem("lang") || "vi";
-
-  await loadLang(lang);
   await loadTheme();
   await loadPosts();
 
   router();
 
-  if (POSTS[0]) {
-    loadPost(POSTS[0].slug);
-  }
-
-  // LANGUAGE
-  const langSwitch = document.getElementById("lang-switch");
-
-  ["vi", "en"].forEach(l => {
-    const opt = document.createElement("option");
-    opt.value = l;
-    opt.textContent = l;
-    langSwitch.appendChild(opt);
-  });
-
-  langSwitch.value = lang;
-
-  langSwitch.onchange = async () => {
-    await loadLang(langSwitch.value);
-    router();
-  };
-
-  // SEARCH
   const search = document.getElementById("search");
 
   search.oninput = () => {
-    if (!search.value) {
-      renderPosts();
-    } else {
-      searchPosts(search.value);
-    }
+    searchPosts(search.value);
   };
 })();
