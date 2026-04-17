a0012a79a7bb2a2c9746bbede6fe246676136d4f
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-17 18:07:56 +0700

    Create router.js

---
+++ assets/js/router.js
@@ -0,0 +1,15 @@
+function initRouter(){
+  window.addEventListener("hashchange", render);
+  render();
+}
+
+async function render(){
+  const route = location.hash.slice(2);
+
+  if(route.startsWith("post/")) return renderPost(route.split("/")[1]);
+  if(route.startsWith("doc/")) return renderDoc(route.split("/")[1]);
+  if(route === "blog") return renderBlog();
+  if(route === "docs") return renderDocs();
+
+  document.getElementById("app").innerHTML = `<h2>${t('welcome')}</h2>`;
+}
