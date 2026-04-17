cb15203106a322075333174bc41c42f93b619d18
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-17 17:38:42 +0700

    Update markdown.js

--- assets/js/markdown.js
+++ assets/js/markdown.js
@@ -1,8 +1,11 @@
 function parseMarkdown(md){
   return md
-    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
-    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
-    .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
-    .replace(/\*(.*)\*/gim, '<i>$1</i>')
-    .replace(/\n/g, '<br>');
-} 
+    .replace(/^### (.*$)/gim,'<h3>$1</h3>')
+    .replace(/^## (.*$)/gim,'<h2>$1</h2>')
+    .replace(/^# (.*$)/gim,'<h1>$1</h1>')
+    .replace(/```([\s\S]*?)```/gim,'<pre><code>$1</code></pre>')
+    .replace(/!\[(.*?)\]\((.*?)\)/gim,'<img src="$2" alt="$1" loading="lazy">')
+    .replace(/\*\*(.*?)\*\*/gim,'<b>$1</b>')
+    .replace(/\*(.*?)\*/gim,'<i>$1</i>')
+    .replace(/\n/g,'<br>');
+}
