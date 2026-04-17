277c365e3cb20c4aeeea206cc3a29855c8d84f49
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-17 14:51:53 +0700

    Create router.js

---
+++ router.js
@@ -0,0 +1,18 @@
+const routes = {
+  "/": homePage,
+  "/post": postPage
+};
+
+function navigate(path, data) {
+  history.pushState(data, "", path);
+  renderRoute();
+}
+
+window.onpopstate = renderRoute;
+
+function renderRoute() {
+  const path = location.pathname;
+  const view = routes[path] || homePage;
+  document.getElementById("app").innerHTML = "";
+  view();
+} 
