b73418b8924274f2eb854fcec1990ce7a3847c5f
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-17 17:54:30 +0700

    Delete assets/js directory

--- assets/js/storage.js
+++
@@ -1,7 +0,0 @@
-function setCache(key, value){
-  localStorage.setItem('cache_'+key, value);
-}
-
-function getCache(key){
-  return localStorage.getItem('cache_'+key);
-}
--- assets/js/theme.js
+++
@@ -1,9 +0,0 @@
-function initTheme(){
-  const current = localStorage.getItem('theme');
-  if(current === 'light') document.body.classList.add('light');
-
-  document.getElementById('theme-toggle').onclick = ()=>{
-    document.body.classList.toggle('light');
-    localStorage.setItem('theme', document.body.classList.contains('light')?'light':'dark');
-  };
-}
