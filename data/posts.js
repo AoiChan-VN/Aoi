7ee879b3263d5a3d06df9010cdbe27ad8fb04f86
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-24 18:07:42 +0700

    Create posts.js with initial post data
    Added an array of post objects with titles, descriptions, thumbnails, and associated files.

---
+++ data/posts.js
@@ -0,0 +1,38 @@
+export const posts = [
+{
+id: 1,
+title: "📢《Thông báo》",
+desc: "➥ Ra mắt phiên bản chính thức",
+thumb: "./assets/img/thumb-1.webp",
+file: "broadcast.md"
+},
+{
+id: 2,
+title: "📦《Plugins》",
+desc: "➥ Spigot | PaperMC",
+thumb: "./assets/img/thumb-2.webp",
+file: "plugin.md"
+},
+{
+id: 3,
+title: "🔧《Cập nhập》",
+desc: "➥ Nâng cao trải nghiệm",
+thumb: "./assets/img/thumb-3.webp",
+file: "update.md"
+},
+{
+id: 4,
+title: "📖《Giới thiệu》",
+desc: "➥ Xin chào các 「Đạo hữu」",
+thumb: "./assets/img/thumb-4.webp",
+file: "info.md"
+},
+{
+id: 5,
+title: "《Thử nghiệm》",
+desc: "➥ 〈No data〉",
+thumb: "./assets/img/thumb-5.webp",
+file: "posts.md"
+}
+];
+ 
