1d0876b467b54bf76dab96a2234fb32e4d1433f6
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-27 23:45:23 +0700

    Fix thumbnail paths in posts.js
    Updated thumbnail paths in posts.js to use the correct directory.

--- data/posts.js
+++ data/posts.js
@@ -3,35 +3,35 @@ export const posts = [
 id: 1,
 title: "📢《Thông báo》",
 desc: "➥ Ra mắt phiên bản chính thức",
-thumb: "./assets/img/posters/Poster.webp",
+thumb: "./assets/posters/Poster.webp",
 file: "broadcast.md"
 },
 {
 id: 2,
 title: "📦《Plugins》",
 desc: "➥ Spigot | PaperMC",
-thumb: "./assets/img/posters/Poster.webp",
+thumb: "./assets/posters/Poster.webp",
 file: "plugin.md"
 },
 {
 id: 3,
 title: "🔧《Cập nhập》",
 desc: "➥ Nâng cao trải nghiệm",
-thumb: "./assets/img/posters/Poster.webp",
+thumb: "./assets/posters/Poster.webp",
 file: "update.md"
 },
 {
 id: 4,
 title: "📖《Giới thiệu》",
 desc: "➥ Xin chào các 「Đạo hữu」",
-thumb: "./assets/img/posters/Poster.webp",
+thumb: "./assets/posters/Poster.webp",
 file: "info.md"
 },
 {
 id: 5,
 title: "《Thử nghiệm》",
 desc: "➥ 〈No data〉",
-thumb: "./assets/img/posters/Poster.webp",
+thumb: "./assets/posters/Poster.webp",
 file: "posts.md"
 }
 ];
