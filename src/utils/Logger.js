9016803d60c7b9b9fdd5f328582f5b59fdc113a9
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-28 18:35:24 +0700

    Delete src directory

--- src/utils/Logger.js
+++
@@ -1,9 +0,0 @@
-export const Logger = {
-    error: (msg, detail = '') => {
-        console.error(`[AoiApp Error]: ${msg}`, detail);
-        // Có thể mở rộng để gửi log về server
-    },
-    info: (msg) => {
-        console.log(`%c[AoiApp]: ${msg}`, 'color: #7c7cff; font-weight: bold');
-    }
-};
